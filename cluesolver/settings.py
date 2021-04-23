56c569bd862b2e65c351f142417ac14d6e68b4f1
David Breese <dabreese00@gmail.com>
2021-04-22 23:15:20 -0500

    Improve development config/tooling
    - Add staticfiles/ to gitignore, for whitenoise.
    - Update settings.py to ensure runserver uses whitenoise. [1]
    - Update requirements.txt to use pip-tools, and layer dev requirements.
    - Add some low priority items to todo.txt.
    
    [1] https://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development

--- cluesolver/settings.py
+++ cluesolver/settings.py
@@ -21,6 +21,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
