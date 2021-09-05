8a5d03e9088803ea0f71e7a8028ee73e2a277201
David Breese <dabreese00@gmail.com>
2021-09-05 10:31:29 -0500

    Fix bug with users API view

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 from api import views
 
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
-app_name = 'api'
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls',
